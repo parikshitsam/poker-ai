a13f0b7713c1bc48f647efbc377e5bafceaec8ec
parikshitsam <63334190+parikshitsam@users.noreply.github.com>
2021-04-03 14:16:17 +0530

    Update prob_winning.py
    completed prob_function

--- prob_winning.py
+++ prob_winning.py
@@ -4,7 +4,26 @@ Created on Sat Apr  3 13:58:28 2021
 
 @author: parik
 """
+## ***Post River***
 
-def prob_winning(hand):
-    """Takes hand as given
+def prob_winning(hand1, comm_cards):
+    """Takes hand1 as given
     Returns prob of winning"""
+    all_hands = list_of_all_possible_hands()
+    
+    win_list = []
+    
+    counter = 0
+    
+    for hand in all_hands:
+        if better_than(hand1,hand):
+            win_list[counter] = 1
+        else:
+            win_list[counter] = 0
+        
+        counter+= 1
+    
+    p_winning = sum(win_list)/len(win_list)
+    
+    return p_winning
+    
